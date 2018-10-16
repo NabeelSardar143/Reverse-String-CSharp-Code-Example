6ab3ee40e150b85f618bcf986c92bf1d49659248
NabeelSardar <nabeeelsardar143@gmail.com>
2018-10-16 11:24:03 +0500

    First commit

---
+++ Stringreverse/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stringreverse
+{
+    class Search
+    {
+        String func(String el)
+        {
+            int length=(el.Length)-1;
+            String revresstring = "";
+           for(;length>=0 ;length--)
+            {
+                revresstring =revresstring+ el[length];
+            }
+            return revresstring;
+            
+        }
+        static void Main(string[] args)
+        {
+            Search obj=new Search();
+            Console.WriteLine("Enter a String");
+            String el= Console.ReadLine();
+            Console.WriteLine("Rverse String");
+            Console.WriteLine(obj.func(el));
+           
+            Console.ReadKey();
+            
+        }
+    }
+}
